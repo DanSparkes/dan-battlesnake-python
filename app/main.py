02a9d95535fa5f816e55e6b9b3647d87f157fc09
dan <yell@dracu.la>
2015-03-02 22:25:36 -0800

    More Testing

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def start():
     data = bottle.request.json
 
     return json.dumps({
-        'name': 'battlesnake-python',
+        'name': 'Dan\'s Snake',
         'color': '#000000',
         'head_url': 'http://battlesnake-python.herokuapp.com',
         'taunt': 'ArGG!'
@@ -26,12 +26,26 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+    
+    if(getSnakeHead()[0] < data["board"][0]):
+        return json.dumps({
+            'move': 'right',
+            'taunt': 'ArGG!'
+        })
+    else:
+        return json.dumps({
+            'move': 'right',
+            'taunt': 'ArGG!'
+        })
 
-    return json.dumps({
-        'move': 'right',
-        'taunt': 'ArGG!'
-    })
+def getSnakeIndex(snakes, name):
+    len = snakes.length
+    for x in range(0, len):
+        if(snake[x].name == name):
+            return x
 
+def getSnakeHead():
+    return data["snakes"][getSnakeIndex(data["snakes"], "Dan\'s Snake")][0]
 
 @bottle.post('/end')
 def end():
